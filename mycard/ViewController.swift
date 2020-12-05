69f385b817ba6421fc6e87d48d3efc82b44ca34a
Joseph Maclean Arhin <josephmacleanarhin@outlook.com>
2020-12-05 19:10:58 +0000

    Initial Commit

---
+++ mycard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mycard
+//
+//  Created by Joseph Maclean Arhin on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
